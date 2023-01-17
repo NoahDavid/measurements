88787bdbc26d7743bb37c76bd6ab2311ad56a38b
NoahDavid <photonicquark@yahoo.com>
2023-01-17 13:16:21 -0500

    I think I fixed all issues, releasing other version

--- measurements/Measurement.py
+++ measurements/Measurement.py
@@ -9,12 +9,15 @@ class Measurement:
         else:
             self.val = Decimal(val)
             self.unc = Decimal(unc)
-
-    def in_range(self, num):
-        return self.val + self.unc > num and self.val + self.unc < num
-
-    def error_bar_widths(self, num):
-        return abs(num - self.val) / self.unc
+    
+    ############################# Printing #############################
+    
+    def  __str__(self):
+        a = self._round_result(self)
+        return f'{a.val} ± {a.unc}'
+    
+    def __repr__(self):
+        return self.__str__()
 
     def _round_result(self, obj):
         if type(obj.val) == Decimal and type(obj.unc) == Decimal:
@@ -27,7 +30,18 @@ class Measurement:
             return Measurement(val, unc)
         else:
             return obj
+    
+    def round_unc(self, places):
+        rounded = self._round_result(self)
+        self.unc = rounded.unc
+        self.val = rounded.val
 
+    ############################# Arithmetic #############################
+
+    ### Multiplication ###
+
+    # z = x * y
+    # dz = sqrt((dx * y)**2 + (x * dy)**2)
     def __mul__(self, other):
         if type(other) == Measurement:
             val = other.val * self.val
@@ -39,7 +53,11 @@ class Measurement:
 
     def __rmul__(self, other):
         return self.__mul__(other)
+
+    ### Division ###
     
+    # z = x / y
+    # dz = sqrt((dx / y)**2 + (x * dy / y**2)**2)
     def __truediv__(self, other):
         if type(other) == Measurement:
             val = self.val / other.val
@@ -49,6 +67,19 @@ class Measurement:
         else:
             return Measurement(self.val / Decimal(other), self.unc / Decimal(other))
     
+    def __rtruediv__(self, other):
+        if type(other) == Measurement:
+            val = other.val / self.val
+            unc = math.sqrt((other.unc / self.val)**2 + (self.unc * other.val / (self.val**2))**2)
+
+            return Measurement(val, unc)
+        else:
+            return Measurement(other, '0').__truediv__(self)
+
+    ### Addition ###
+
+    # z = x + y
+    # dz = sqrt(dx**2 + dy**2)
     def __add__(self, other):
         if type(other) == Measurement:
             unc = math.sqrt(other.unc**2 + self.unc**2)
@@ -57,6 +88,13 @@ class Measurement:
         else:
             return Measurement(self.val + Decimal(other), self.unc)
     
+    def __radd__(self, other):
+        return self.__add__(other)
+    
+    ### Subtraction ###
+
+    # z = x - y
+    # dz = sqrt(dx**2 + dy**2)
     def __sub__(self, other):
         if type(other) == Measurement:
             unc = math.sqrt(other.unc**2 + self.unc**2)
@@ -65,6 +103,18 @@ class Measurement:
         else:
             return Measurement(self.val - Decimal(other), self.unc)
     
+    def __rsub__(self, other):
+        if type(other) == Measurement:
+            unc = math.sqrt(other.unc**2 + self.unc**2)
+
+            return Measurement(other.val - self.val, unc)
+        else:
+            return Measurement(Decimal(other) - self.val, self.unc)
+    
+    ### Exponentiation ###
+
+    # z = x**y
+    # dz = sqrt((dy * ln(x) * z)**2 + (dx * y / x * z)**2)
     def __pow__(self, other):
         if type(other) == Measurement:
             val = self.val**other.val
@@ -73,7 +123,7 @@ class Measurement:
             return Measurement(val, unc)
         else:
             return Measurement(self.val**Decimal(other), self.unc * Decimal(other) / self.val * self.val**Decimal(other))
-    
+
     def __rpow__(self, other):
         if type(other) == Measurement:
             val = other.val**self.val
@@ -83,6 +133,112 @@ class Measurement:
         else:
             return Measurement(Decimal(other)**self.val, self.unc * Decimal(other) / self.val * Decimal(other)**self.val)
     
+    ############################# Math functions #############################
+
+    ### Trigonometric functions ###
+    # sin, cos, tan, asin, acos, atan, atan2, sinh, cosh, tanh, asinh, acosh, and atanh are included.
+
+    # z = cos(x)
+    # dz = abs(-sin(x) * dx)
+    def cos(self):
+        return Measurement(math.cos(self.val), abs(float(self.unc) * math.sin(self.val)))
+    
+    # z = sin(x)
+    # dz = abs(cos(x) * dx)
+    def sin(self):
+        return Measurement(math.sin(self.val), abs(float(self.unc) * math.cos(self.val)))
+    
+    # z = tan(x)
+    # dz = abs((1 + tan(x)**2) * dx)
+    def tan(self):
+        return Measurement(math.tan(self.val), abs(float(self.unc) / math.cos(self.val)**2))
+
+    # z = acos(x)
+    # dz = abs(-1 / sqrt(1 - x**2) * dx)
+    def acos(self):
+        return Measurement(math.acos(self.val), abs(float(self.unc) / math.sqrt(1 - self.val**2)))
+    
+    # z = asin(x)
+    # dz = abs(1 / sqrt(1 - x**2) * dx)
+    def asin(self):
+        return Measurement(math.asin(self.val), abs(float(self.unc) / math.sqrt(1 - self.val**2)))
+
+    # z = atan(x)
+    # dz = abs(1 / (1 + x**2) * dx)
+    def atan(self):
+        return Measurement(math.atan(self.val), abs(self.unc / (1 + self.val**2)))
+    
+    # z = atan2(y, x)
+    # dz = abs(1 / (x**2 + y**2) * dx)
+    def atan2(self, other):
+        if type(other) == Measurement:
+            val = math.atan2(self.val, other.val)
+            unc = math.sqrt((self.unc / (self.val**2 + other.val**2))**2 + (other.unc / (self.val**2 + other.val**2))**2)
+
+            return Measurement(val, unc)
+        else:
+            return Measurement(math.atan2(self.val, Decimal(other)), self.unc / (self.val**2 + Decimal(other)**2))
+
+    # z = cosh(x)
+    # dz = abs(sinh(x) * dx)
+    def cosh(self):
+        return Measurement(math.cosh(self.val), abs(float(self.unc) * math.sinh(self.val)))
+
+    # z = sinh(x)
+    # dz = abs(cosh(x) * dx)
+    def sinh(self):
+        return Measurement(math.sinh(self.val), abs(float(self.unc) * math.cosh(self.val)))
+
+    # z = tanh(x)
+    # dz = abs((1 - tanh(x)**2) * dx)
+    def tanh(self):
+        return Measurement(math.tanh(self.val), abs(float(self.unc) / math.cosh(self.val)**2))
+
+    # z = acosh(x)
+    # dz = abs(1 / sqrt(x**2 - 1) * dx)
+    def acosh(self):
+        return Measurement(math.acosh(self.val), abs(float(self.unc) / math.sqrt(self.val**2 - 1)))
+
+    # z = asinh(x)
+    # dz = abs(1 / sqrt(x**2 + 1) * dx)
+    def asinh(self):
+        return Measurement(math.asinh(self.val), abs(float(self.unc) / math.sqrt(self.val**2 + 1)))
+
+    # z = atanh(x)
+    # dz = abs(1 / (1 - x**2) * dx)
+    def atanh(self):
+        return Measurement(math.atanh(self.val), abs(self.unc / (1 - self.val**2)))
+
+    ### Other functions ###
+    # exp, log, log10, sqrt, and abs are included.
+
+    # z = exp(x)
+    # dz = abs(exp(x) * dx)
+    def exp(self):
+        return Measurement(math.exp(self.val), abs(float(self.unc) * math.exp(self.val)))
+
+    # z = log(x)
+    # dz = abs(1 / x * dx)
+    def log(self):
+        return Measurement(math.log(self.val), abs(self.unc / self.val))
+
+    # z = log10(x)
+    # dz = abs(1 / (x * log(10)) * dx)
+    def log10(self):
+        return Measurement(math.log10(self.val), abs(float(self.unc) / (float(self.val) * math.log(10))))
+
+    # z = sqrt(x)
+    # dz = abs(1 / (2 * sqrt(x)) * dx)
+    def sqrt(self):
+        return Measurement(math.sqrt(self.val), abs(float(self.unc) / (2 * math.sqrt(self.val))))
+
+    # z = abs(x)
+    # dz = abs(dx)
+    def abs(self):
+        return Measurement(abs(self.val), self.unc)
+    
+    ############################# Comparison Operators #############################
+
     def __eq__(self, other):
         if type(other) == Measurement:
             return self.val == other.val and self.unc == other.unc
@@ -118,55 +274,3 @@ class Measurement:
             return self.val >= other.val
         else:
             return self.val >= Decimal(other)
-    
-    def cos(self):
-        return Measurement(math.cos(self.val), self.unc * math.sin(self.val))
-    
-    def sin(self):
-        return Measurement(math.sin(self.val), self.unc * math.cos(self.val))
-    
-    def tan(self):
-        return Measurement(math.tan(self.val), self.unc / math.cos(self.val)**2)
-    
-    def acos(self):
-        return Measurement(math.acos(self.val), self.unc / math.sqrt(1 - self.val**2))
-    
-    def asin(self):
-        return Measurement(math.asin(self.val), self.unc / math.sqrt(1 - self.val**2))
-    
-    def atan(self):
-        return Measurement(math.atan(self.val), self.unc / (1 + self.val**2))
-    
-    def cosh(self):
-        return Measurement(math.cosh(self.val), self.unc * math.sinh(self.val))
-    
-    def sinh(self):
-        return Measurement(math.sinh(self.val), self.unc * math.cosh(self.val))
-    
-    def tanh(self):
-        return Measurement(math.tanh(self.val), self.unc / math.cosh(self.val)**2)
-    
-    def acosh(self):
-        return Measurement(math.acosh(self.val), self.unc / math.sqrt(self.val**2 - 1))
-
-    def asinh(self):
-        return Measurement(math.asinh(self.val), self.unc / math.sqrt(self.val**2 + 1))
-
-    def atanh(self):
-        return Measurement(math.atanh(self.val), self.unc / (1 - self.val**2))
-
-    def exp(self):
-        return Measurement(math.exp(self.val), self.unc * math.exp(self.val))
-
-    def log(self):
-        return Measurement(math.log(self.val), self.unc / self.val)
-
-    def sqrt(self):
-        return Measurement(math.sqrt(self.val), self.unc / (2 * math.sqrt(self.val)))
-    
-    def  __str__(self):
-        a = self._round_result(self)
-        return f'{a.val} ± {a.unc}'
-    
-    def __repr__(self):
-        return self.__str__()
--- test.py
+++ test.py
@@ -1,5 +1,100 @@
 from measurements import Measurement as M
 
-m = M('45', '2') ** M('2', '0.1')
+# Run tests on all the parts of the Measurement class
 
-print(m)
+# Test the constructor
+assert M(1, 0.1) == M(1, 0.1)
+assert M(1, 0.1) != M(1, 0.2)
+assert M(1, 0.1) != M(2, 0.1)
+assert M(1, 0.1) != M(2, 0.2)
+
+
+# Test the __str__ method with pretty printing
+print(f'1 ± 0.1 = {M(1, 0.1)}')
+
+# Test the __repr__ method with pretty printing
+print(f'1 ± 0.1 = {repr(M(1, 0.1))}')
+
+# Test the __add__ method with pretty printing
+print(f'1 ± 0.1 + 1 ± 0.1 = {M(1, 0.1) + M(1, 0.1)}')
+
+# Test the __sub__ method with pretty printing
+print(f'1 ± 0.1 - 1 ± 0.1 = {M(1, 0.1) - M(1, 0.1)}')
+
+# Test the __mul__ method with pretty printing
+print(f'1 ± 0.1 * 1 ± 0.1 = {M(1, 0.1) * M(1, 0.1)}')
+
+# Test the __truediv__ method with pretty printing
+print(f'1 ± 0.1 / 1 ± 0.1 = {M(1, 0.1) / M(1, 0.1)}')
+
+# Test the __pow__ method with pretty printing
+print(f'1 ± 0.1 ** 1 ± 0.1 = {M(1, 0.1) ** M(1, 0.1)}')
+
+# Test the __radd__ method with pretty printing
+print(f'1 + 1 ± 0.1 = {1 + M(1, 0.1)}')
+
+# Test the __rsub__ method with pretty printing
+print(f'1 - 1 ± 0.1 = {1 - M(1, 0.1)}')
+
+# Test the __rmul__ method with pretty printing
+print(f'1 * 1 ± 0.1 = {1 * M(1, 0.1)}')
+
+# Test the __rtruediv__ method with pretty printing
+print(f'1 / 1 ± 0.1 = {1 / M(1, 0.1)}')
+
+# Test the __rpow__ method with pretty printing
+print(f'1 ** 1 ± 0.1 = {1 ** M(1, 0.1)}')
+
+# Test the cos method with pretty printing
+print(f'cos(1 ± 0.1) = {M(1, 0.1).cos()}')
+
+# Test the sin method with pretty printing
+print(f'sin(1 ± 0.1) = {M(1, 0.1).sin()}')
+
+# Test the tan method with pretty printing
+print(f'tan(1 ± 0.1) = {M(1, 0.1).tan()}')
+
+# Test the acos method with pretty printing
+print(f'acos(0.5 ± 0.1) = {M(0.5, 0.1).acos()}')
+
+# Test the asin method with pretty printing
+print(f'asin(0.5 ± 0.1) = {M(0.5, 0.1).asin()}')
+
+# Test the atan method with pretty printing
+print(f'atan(0.5 ± 0.1) = {M(0.5, 0.1).atan()}')
+
+# Test the atan2 method with pretty printing
+print(f'atan2(0.5 ± 0.1, 1 ± 0.1) = {M(0.5, 0.1).atan2(M(1, 0.1))}')
+
+# Test the cosh method with pretty printing
+print(f'cosh(1 ± 0.1) = {M(1, 0.1).cosh()}')
+
+# Test the sinh method with pretty printing
+print(f'sinh(1 ± 0.1) = {M(1, 0.1).sinh()}')
+
+# Test the tanh method with pretty printing
+print(f'tanh(1 ± 0.1) = {M(1, 0.1).tanh()}')
+
+# Test the acosh method with pretty printing
+print(f'acosh(1.5 ± 0.1) = {M(1.5, 0.1).acosh()}')
+
+# Test the asinh method with pretty printing
+print(f'asinh(0.5 ± 0.1) = {M(0.5, 0.1).asinh()}')
+
+# Test the atanh method with pretty printing
+print(f'atanh(0.5 ± 0.1) = {M(0.5, 0.1).atanh()}')
+
+# Test the exp method with pretty printing
+print(f'exp(1 ± 0.1) = {M(1, 0.1).exp()}')
+
+# Test the log method with pretty printing
+print(f'log(1 ± 0.1) = {M(1, 0.1).log()}')
+
+# Test the log10 method with pretty printing
+print(f'log10(1 ± 0.1) = {M(1, 0.1).log10()}')
+
+# Test the sqrt method with pretty printing
+print(f'sqrt(1 ± 0.1) = {M(1, 0.1).sqrt()}')
+
+# Test the abs method with pretty printing
+print(f'abs(-1 ± 0.1) = {M(-1, 0.1).abs()}')
