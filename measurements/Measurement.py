cbc37093a1ad35f3f63d6e1535629af80ba4be21
NoahDavid <photonicquark@yahoo.com>
2023-01-17 11:18:43 -0500

    A bunch of functions added for convenience

--- measurements/Measurement.py
+++ measurements/Measurement.py
@@ -1,4 +1,4 @@
-import numpy as np
+import math
 from decimal import Decimal, DecimalTuple
 
 class Measurement:
@@ -23,6 +23,7 @@ class Measurement:
             a = DecimalTuple(a.sign, (a.digits[0], ), a.exponent + len(a.digits) - 1)
             unc = Decimal(a)
             val = round(obj.val, -1 * unc.as_tuple().exponent)
+            unc = round(obj.unc, -1 * unc.as_tuple().exponent)
             return Measurement(val, unc)
         else:
             return obj
@@ -30,11 +31,11 @@ class Measurement:
     def __mul__(self, other):
         if type(other) == Measurement:
             val = other.val * self.val
-            unc = np.sqrt((other.unc * self.val)**2 + (self.unc * other.val)**2)
+            unc = math.sqrt((other.unc * self.val)**2 + (self.unc * other.val)**2)
 
             return Measurement(val, unc)
         else:
-            return Measurement(other * self.val, other * self.unc)
+            return Measurement(Decimal(other) * self.val, Decimal(other) * self.unc)
 
     def __rmul__(self, other):
         return self.__mul__(other)
@@ -42,27 +43,126 @@ class Measurement:
     def __truediv__(self, other):
         if type(other) == Measurement:
             val = self.val / other.val
-            unc = np.sqrt((other.unc * self.val / (other.val**2))**2 + (self.unc / other.val)**2)
+            unc = math.sqrt((other.unc * self.val / (other.val**2))**2 + (self.unc / other.val)**2)
 
             return Measurement(val, unc)
         else:
-            return Measurement(self.val / other, self.unc / other)
+            return Measurement(self.val / Decimal(other), self.unc / Decimal(other))
     
     def __add__(self, other):
         if type(other) == Measurement:
-            unc = np.sqrt(other.unc**2 + self.unc**2)
+            unc = math.sqrt(other.unc**2 + self.unc**2)
 
             return Measurement(other.val + self.val, unc)
         else:
-            return Measurement(self.val + other, self.unc)
+            return Measurement(self.val + Decimal(other), self.unc)
     
     def __sub__(self, other):
         if type(other) == Measurement:
-            unc = np.sqrt(other.unc**2 + self.unc**2)
+            unc = math.sqrt(other.unc**2 + self.unc**2)
 
             return Measurement(self.val - other.val, unc)
         else:
-            return Measurement(self.val - other, self.unc)
+            return Measurement(self.val - Decimal(other), self.unc)
+    
+    def __pow__(self, other):
+        if type(other) == Measurement:
+            val = self.val**other.val
+            unc = math.sqrt((other.unc * Decimal(math.log(self.val)) * val)**2 + (self.unc * other.val / self.val * val)**2)
+
+            return Measurement(val, unc)
+        else:
+            return Measurement(self.val**Decimal(other), self.unc * Decimal(other) / self.val * self.val**Decimal(other))
+    
+    def __rpow__(self, other):
+        if type(other) == Measurement:
+            val = other.val**self.val
+            unc = math.sqrt((other.unc * Decimal(math.log(self.val)) * val)**2 + (self.unc * other.val / self.val * val)**2)
+
+            return Measurement(val, unc)
+        else:
+            return Measurement(Decimal(other)**self.val, self.unc * Decimal(other) / self.val * Decimal(other)**self.val)
+    
+    def __eq__(self, other):
+        if type(other) == Measurement:
+            return self.val == other.val and self.unc == other.unc
+        else:
+            return self.val == Decimal(other) and self.unc == Decimal(0)
+    
+    def __ne__(self, other):
+        if type(other) == Measurement:
+            return self.val != other.val or self.unc != other.unc
+        else:
+            return self.val != Decimal(other) or self.unc != Decimal(0)
+
+    def __lt__(self, other):
+        if type(other) == Measurement:
+            return self.val < other.val
+        else:
+            return self.val < Decimal(other)
+    
+    def __le__(self, other):
+        if type(other) == Measurement:
+            return self.val <= other.val
+        else:
+            return self.val <= Decimal(other)
+    
+    def __gt__(self, other):
+        if type(other) == Measurement:
+            return self.val > other.val
+        else:
+            return self.val > Decimal(other)
+    
+    def __ge__(self, other):
+        if type(other) == Measurement:
+            return self.val >= other.val
+        else:
+            return self.val >= Decimal(other)
+    
+    def cos(self):
+        return Measurement(math.cos(self.val), self.unc * math.sin(self.val))
+    
+    def sin(self):
+        return Measurement(math.sin(self.val), self.unc * math.cos(self.val))
+    
+    def tan(self):
+        return Measurement(math.tan(self.val), self.unc / math.cos(self.val)**2)
+    
+    def acos(self):
+        return Measurement(math.acos(self.val), self.unc / math.sqrt(1 - self.val**2))
+    
+    def asin(self):
+        return Measurement(math.asin(self.val), self.unc / math.sqrt(1 - self.val**2))
+    
+    def atan(self):
+        return Measurement(math.atan(self.val), self.unc / (1 + self.val**2))
+    
+    def cosh(self):
+        return Measurement(math.cosh(self.val), self.unc * math.sinh(self.val))
+    
+    def sinh(self):
+        return Measurement(math.sinh(self.val), self.unc * math.cosh(self.val))
+    
+    def tanh(self):
+        return Measurement(math.tanh(self.val), self.unc / math.cosh(self.val)**2)
+    
+    def acosh(self):
+        return Measurement(math.acosh(self.val), self.unc / math.sqrt(self.val**2 - 1))
+
+    def asinh(self):
+        return Measurement(math.asinh(self.val), self.unc / math.sqrt(self.val**2 + 1))
+
+    def atanh(self):
+        return Measurement(math.atanh(self.val), self.unc / (1 - self.val**2))
+
+    def exp(self):
+        return Measurement(math.exp(self.val), self.unc * math.exp(self.val))
+
+    def log(self):
+        return Measurement(math.log(self.val), self.unc / self.val)
+
+    def sqrt(self):
+        return Measurement(math.sqrt(self.val), self.unc / (2 * math.sqrt(self.val)))
     
     def  __str__(self):
         a = self._round_result(self)
---
+++ test.py
@@ -0,0 +1,5 @@
+from measurements import Measurement as M
+
+m = M('45', '2') ** M('2', '0.1')
+
+print(m)
