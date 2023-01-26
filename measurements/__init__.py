254b8165dba0f0dc1db12c56883ad56f18b6e0fb
NoahDavid <photonicquark@yahoo.com>
2023-01-26 17:31:15 -0500

    Incremented version

--- measurements/__init__.py
+++ measurements/__init__.py
@@ -2,7 +2,7 @@
 This module contains the Measurement class and all the subclasses.
 '''
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Noah Koliadko'
 __credits__ = 'Andrews University'
 
